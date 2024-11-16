374f2e04c8efac6dcda547aa789059329552e68e
SomeRandomPerson4112 <ayaangadani@gmail.com>
2024-11-16 18:52:23 +0500

    mai.12332145680976

---
+++ operators.py
@@ -0,0 +1,11 @@
+a=5
+b=-10
+c=0
+if a > 0 and b>0:
+    print("EITHER NUMBERS ARE GREATER THAN 0")
+else:
+    print("no number is greater than 0")
+if c > 0 and b>0:
+    print("EITHER NUMBERS ARE GREATER THAN 0")
+else:
+    print("no number is greater than 0")
